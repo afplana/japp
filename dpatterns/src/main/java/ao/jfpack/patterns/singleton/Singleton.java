92f7f126a5d6b604c0a2315101f9842a071d891e
aklan <aplanas9009@gmail.com>
2020-08-22 15:14:32 +0300

    Make Singleton pattern example thread safe

--- dpatterns/src/main/java/ao/jfpack/patterns/singleton/Singleton.java
+++ dpatterns/src/main/java/ao/jfpack/patterns/singleton/Singleton.java
@@ -5,16 +5,20 @@ import java.util.logging.Logger;
 
 public class Singleton {
 
-    private static final Singleton instance = new Singleton();
-
-    Logger log = Logger.getLogger(this.getClass().getName());
+    private static final Logger log = Logger.getLogger(Singleton.class.getName());
+    private static Singleton instance;
 
     private Singleton(){
     }
 
     private static Singleton getInstance() {
-        if(Objects.isNull(instance))
-            return new Singleton();
+        // The following code snippet provides the double-checked locking implementation.
+        if(Objects.isNull(instance)) {
+            synchronized (Singleton.class) {
+                if (Objects.isNull(instance))
+                    return new Singleton();
+            }
+        }
         return instance;
     }
 
